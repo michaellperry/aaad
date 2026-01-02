e662933553cd080968d1ae7dd5bdac26d7dc7798
Michael L Perry <michael@qedcode.com>
2026-01-01 21:12:26 -0600

    Migration for billing and shipping addresses

---
+++ src/GloboTicket.Infrastructure/Data/Migrations/20260102031130_BillingAndShippingAddress.cs
@@ -0,0 +1,155 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GloboTicket.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class BillingAndShippingAddress : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BillingCity",
+                table: "Customers",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "BillingCountry",
+                table: "Customers",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "BillingPostalCode",
+                table: "Customers",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "BillingStateOrProvince",
+                table: "Customers",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "BillingStreetLine1",
+                table: "Customers",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "BillingStreetLine2",
+                table: "Customers",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ShippingCity",
+                table: "Customers",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ShippingCountry",
+                table: "Customers",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ShippingPostalCode",
+                table: "Customers",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ShippingStateOrProvince",
+                table: "Customers",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ShippingStreetLine1",
+                table: "Customers",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ShippingStreetLine2",
+                table: "Customers",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BillingCity",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "BillingCountry",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "BillingPostalCode",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "BillingStateOrProvince",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "BillingStreetLine1",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "BillingStreetLine2",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "ShippingCity",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "ShippingCountry",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "ShippingPostalCode",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "ShippingStateOrProvince",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "ShippingStreetLine1",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "ShippingStreetLine2",
+                table: "Customers");
+        }
+    }
+}
